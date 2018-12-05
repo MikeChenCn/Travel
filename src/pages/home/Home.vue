6f4dffee76ac37c8c97ccb3a6aef345873350316
MikeChenCn <790670714@qq.com>
2018-12-05 21:02:57 +0800

    no message

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<Home-Header></Home-Header>
+	</div>
+	
+</template>
+
+<script>
+	import homeHeader from 'components/Header.vue'
+	export default{
+		name:'Home',
+		component:{
+			HomeHeader
+		}
+	}
+</script>
+
+<style>
+	
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		header
+	</div>
+	
+</template>
+
+<script>
+	export default{
+		name:'HomeHeader'
+	}
+</script>
+
+<style>
+	
+</style>
